4c54fb073514bb0176023f4137de1fee8c6ee27d
kbwhodat <byantalok@yahoo.com>
2024-02-17 18:53:40 -0500

    updating

--- lua/ollama/util.lua
+++ lua/ollama/util.lua
@@ -18,7 +18,8 @@ function util.handle_stream(cb)
 			local file_path = "/tmp/sourceoftrueth.json" -- Specify the path to your output file
 			local file = io.open(file_path, "a") -- Open the file in append mode
 			if file then
-				file:write(vim.json.decode(body).response .. "\n") -- Encode the body to JSON and write to the file
+				vim.api.nvim_notify(vim.json.encode(body), vim.log.levels.INFO, { title = "Ollama" })
+				file:write(vim.json.encode(body).. "\n") -- Encode the body to JSON and write to the file
 				file:close() -- Don't forget to close the file
 			else
 				vim.api.nvim_notify("Failed to open file for writing.", vim.log.levels.ERROR, { title = "Ollama" })
